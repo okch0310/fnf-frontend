3c2d39d93bb6bb45e7a17405f12d63b3ddce2147
KimJeongHyun <ptr1129@hanmail.net>
2022-01-26 17:25:58 +0900

    [Add] 전역 스크롤바 적용

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
 const Main = () => {
-  return <div>Jogho</div>;
+  return (
+    <Scrollbars autoHeight autoHeightMin="100vh">
+      <div>Jogho</div>;
+    </Scrollbars>
+  );
 };
 
 export default Main;
