1bd59417b0a75e01492ed67e99515ea0da4f8d17
KimJeongHyun <ptr1129@hanmail.net>
2022-01-26 13:47:32 +0900

    [Add] 폴더 영역 구분 및 생성, 필요 패키지 설치

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import VerticalNav from './components/Nav/VerticalNav';
 import Main from './pages/Main';
 
 export default function Router() {
   return (
     <BrowserRouter>
+      <VerticalNav />
       <Routes>
         <Route path="/" element={<Main />} />
       </Routes>
---
+++ src/components/Nav/VerticalNav.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export default function VerticalNav() {
+  return <NavContainer />;
+}
+
+const NavContainer = styled.section``;
