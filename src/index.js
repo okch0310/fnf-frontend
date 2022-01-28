f11f8c8f0e8ca9103ada9c8a8848698f4cc0302d
채현 <ogchaehyeon@172-1-7-60.lightspeed.hstntx.sbcglobal.net>
2022-01-28 13:46:43 +0900

    [28기 옥채현]Add:카드 구현 완료
    [28기 옥채현] 카드 컴포넌트 구현 완료

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import Router from './Router';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
+import mixin from './styles/mixin';
 
 ReactDOM.render(
   <>
     <GlobalStyle />
-    <ThemeProvider theme={{ ...theme }}>
+    <ThemeProvider theme={{ ...theme, ...mixin }}>
       <Router />
     </ThemeProvider>
   </>,
