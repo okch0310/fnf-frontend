e899d6de52dd73b01b1070e0221bd4da81e665cf
KimJeongHyun <ptr1129@hanmail.net>
2022-02-12 14:15:16 +0900

    [김정현][Fix] 라우터 수정

--- src/Router.js
+++ src/Router.js
@@ -4,7 +4,6 @@ import WithNav from './components/NavOutlet/WithNav';
 import WithoutNav from './components/NavOutlet/WithoutNav';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Main from './pages/Main/Main';
-import SearchStatus from './pages/Main/Contents/Category/SearchStatus/SearchStatus';
 
 const MAIN_CONTENTS_PATH = ['/category', '/styleranking'];
 
@@ -13,7 +12,7 @@ export default function Router() {
     <BrowserRouter>
       <Routes>
         <Route element={<WithoutNav />}>
-          <Route path="/" element={<SearchStatus />} />
+          <Route path="/" element={<LandingPage />} />
         </Route>
         <Route element={<WithNav />}>
           {MAIN_CONTENTS_PATH.map((item, index) => (
