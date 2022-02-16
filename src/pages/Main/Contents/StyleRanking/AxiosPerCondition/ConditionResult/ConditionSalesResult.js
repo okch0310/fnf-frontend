95cd2023aa5bd68b34245c89aeaff6df3ebda725
Seokho0120@gmail.com <93597794+Seokho0120@users.noreply.github.com>
2022-02-16 16:44:04 +0900

    스타일랭킹 검색 조건 페이지 구현 완료

---
+++ src/pages/Main/Contents/StyleRanking/AxiosPerCondition/ConditionResult/ConditionSalesResult.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import DomesticCharts from './SalesResultCharts/DomesticCharts';
+import GlobalCharts from './SalesResultCharts/GlobalCharts';
+import DomesticPieCharts from './SalesResultCharts/DomesticPieCharts';
+
+export default function ConditionSalesResult() {
+  return (
+    <SalesResultContainer>
+      <ChartsContainer>
+        <DomesticCharts />
+        <GlobalCharts />
+        <DomesticPieCharts />
+      </ChartsContainer>
+    </SalesResultContainer>
+  );
+}
+
+const SalesResultContainer = styled.div`
+  width: 100%;
+  height: 98%;
+  margin-top: 12px;
+`;
+
+const ChartsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  gap: 12px;
+`;
