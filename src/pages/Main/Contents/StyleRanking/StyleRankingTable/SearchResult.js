f06798145319efd369bf6876aaec6b33ca0c5d4e
채현 <ogchaehyeon@ogchaehyeon-ui-MacBookPro.local>
2022-02-17 11:20:47 +0900

    [28기 옥채현] 기초 레이아웃 및 toggle box 구현

---
+++ src/pages/Main/Contents/StyleRanking/StyleRankingTable/SearchResult.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function SearchResult() {
+  return <SearchResultWrapper />;
+}
+
+const SearchResultWrapper = styled.div`
+  height: 500px;
+  background-color: salmon;
+`;
