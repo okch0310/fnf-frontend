390514c9c5746537711732d75cded0443535f854
KimJeongHyun <ptr1129@hanmail.net>
2022-02-21 12:03:24 +0900

    [Modify] 레이아웃 수정

--- src/pages/Main/Contents/StyleRanking/StyleRankingTable/StyleRankingTable.js
+++ src/pages/Main/Contents/StyleRanking/StyleRankingTable/StyleRankingTable.js
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { HierarchyContext } from '../StyleRanking';
+
 import styled from 'styled-components';
 import TopSummary from './StyleRankingTableContents/TopSummary';
 import SearchResult from './StyleRankingTableContents/SearchResult';
 import TotalResult from './StyleRankingTableContents/TotalResult';
 
 export default function StyleRankingTable() {
+  const { expandCondition } = useContext(HierarchyContext);
+
   return (
-    <RankingTableWrapper>
+    <RankingTableWrapper expandCondition={expandCondition}>
       <TopSummary />
       <SearchResult />
       <TotalResult />
@@ -17,7 +21,7 @@ export default function StyleRankingTable() {
 const RankingTableWrapper = styled.section`
   display: flex;
   flex-direction: column;
-  flex-basis: 75%;
+  flex-basis: ${props => (props.expandCondition ? '56%' : '75%')};
   text-align: center;
   // flex-basis:56%;
 `;
